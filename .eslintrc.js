cfd729c06dbbf62ecccfe6475d253c328988cc77
Tharlys Dias <tharlys.dias@senior.com.br>
2026-02-23 08:14:41 -0300

    fix: update ESLint rules for unused variables to include varsIgnorePattern

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,9 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     'no-console': 'off',
   },
   ignorePatterns: ['dist/', 'node_modules/', '*.js'],
